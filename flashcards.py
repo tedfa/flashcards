8785b5a23a0880061df409d4a190ae5b9126ca24
tedfa <williambveale@gmail.com>
2018-04-25 14:32:33 -0400

    removed int() func

--- flashcards.py
+++ flashcards.py
@@ -2,7 +2,7 @@ print("Welcome to Flashfacts!")
 print("\n")
 print("To start new project press 1")
 print("to open existing project press 2")
-item = int(input(" "))
+item = input(" ")
 print("\n")
 
 print("You selected, ")
