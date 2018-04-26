4574b1df01bb3ff70b0fb8dce4191cd77120f2c8
tedfa <williambveale@gmail.com>
2018-04-26 15:26:08 -0400

    for loops to read dict in to associative lists

--- flashcards.py
+++ flashcards.py
@@ -5,9 +5,12 @@ def yaml_loader(filepath):
         data = yaml.load(file_descriptor)
         return data
 
-def yaml_dump(filepath, data):
+def yaml_dump(filepath, data): # Don't actually need this function
     with open(filepath, "w") as file_descriptor:
         yaml.dump(data, file_descriptor)
+
+#def dict_parser(data):
+
 
 print("Welcome to Flashfacts!")
 print("\n")
@@ -15,12 +18,18 @@ filepath = input("What is the name of the card pack you wish to study? ")
 
 
 data = yaml_loader(filepath)
-for x in data:
-    print(x)
 
 
+question_list = []
+ans_list = []
+
+for values in data.values():
+    ans_list.append(values)
+
+for items in data:
+    question_list.append(items)
+
+print(question_list)
+print(ans_list)
 
 
-# def open_proj():
-#
-#
